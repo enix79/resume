c7f298fe5f7d9401ed581594b036de0ff4ff1d20
igor.gonak <igor.gonak@hmmh.de>
2021-12-15 23:12:51 +0100

    Create position cards.

---
+++ components/JobPositionCard.tsx
@@ -0,0 +1,37 @@
+import { Card, Avatar, Typography } from 'antd';
+const { Text } = Typography;
+
+interface JobPositionCardProps {
+    position: string,
+    period: string,
+    company: string,
+    companyLogo?: React.ReactNode,
+    tasks: string[]
+}
+
+export default function JobPositionCard({position, period, company, companyLogo, tasks}: JobPositionCardProps) {
+    const cardTitle = (
+        <>
+            <Text>{`${position} @${company}`}</Text><br/>
+            <Text type="secondary" style={{fontSize: "10px"}}>{period}</Text>
+        </>
+    );
+    return (
+    <Card 
+        title={cardTitle}
+        extra={
+          <Avatar
+            alt="hmmh"
+            src={companyLogo}
+            style={{ border: "1px solid gray"}}
+            size="large"
+            />
+        }
+        style={{ width: 450, margin: 50, borderRadius: "5px" }} 
+      >
+        <ul style={{paddingLeft: 15}}>
+            { tasks.map((task, index) => <li key={index} style={{fontSize: "12px"}}>{task}</li>)}
+        </ul>
+      </Card>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,46 @@
-import { Button, Space, DatePicker, Card } from "antd";
-import { CiCircleFilled } from "@ant-design/icons";
+import JobPositionCard from '../components/JobPositionCard';
+import hmmh_logo from '../assets/hmmh_logo.png';
+import sonepar_logo from '../assets/sonepar_logo.svg';
+import Image from 'next/image';
 
+// TODO: use next/image
 export default function Home() {
-  const onChange = () => {};
   return (
-    <div style={{ padding: 100 }}>
-      <Space direction="vertical">
-        <Button type="primary">Primary Button</Button>
-        <Button type="ghost">Ghost Button</Button>
-        <DatePicker onChange={onChange} />
-        <CiCircleFilled />
-      </Space>
-    </div>
-  );
+    <main>
+      
+      <JobPositionCard
+        company='hmmh'
+        companyLogo={<Image src={hmmh_logo} alt="hmmh"/>}
+        position='Frontend Developer'
+        period='Juni 2021 - heute'
+        tasks={[
+          "Webshopmondernisierung & -weiterentwicklung",
+          "Neuentwicklung der Backoffice-Tools"
+        ]}
+      />
+      <JobPositionCard
+        company='hmmh'
+        companyLogo={<Image src={hmmh_logo} alt="hmmh"/>}
+        position='IT-Spezialist für PIM/MAM-Systeme'
+        period='Juni 2019 - Juni 2021'
+        tasks={[
+          "Konfiguration des PIM-Systems Contentserv",
+          "Schnittstellenentwicklung & ETL",
+          "Automatisierung der Prozesse",
+          "Projektmanagement, Kundenberatung, Support, Schulungen"
+        ]}
+      />
+      <JobPositionCard
+        company='Sonepar PIM solutions'
+        companyLogo={<Image src={sonepar_logo} alt="sonepar"/>}
+        position='Software Debeloperr'
+        period='Februar 2017 - Juni 2019'
+        tasks={[
+          "ETL & Datenbanken",
+          "Monitoring & Reporting",
+          "Datenanalysen"
+        ]}
+      />
+    </main>
+  )
 }
